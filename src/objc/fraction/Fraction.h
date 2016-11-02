606403cc268055c530d2323643579e268d57f3f7
furzoom <furzoommn@gmail.com>
2016-11-02 17:51:40 +0800

    remove setter and getter

--- src/objc/fraction/Fraction.h
+++ src/objc/fraction/Fraction.h
@@ -7,9 +7,5 @@
 @property int numerator, denominator;
 
 - (void) print;
-- (void) setNumerator: (int) n;
-- (void) setDenominator: (int) d;
-- (int) numerator;
-- (int) denominator;
 - (double) convertToNum;
 @end
